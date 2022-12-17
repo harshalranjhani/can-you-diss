d357b565fefab90feed0f7cd8aaa1c98217c0a5b
Nakshatra Raghav <combatrickshaw@gmail.com>
2022-12-17 21:31:33 +0530

    post creation page audio file + photo upload

---
+++ assets/Icons.js
@@ -0,0 +1,50 @@
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import MessageOutlinedIcon from "@mui/icons-material/MessageOutlined";
+import BookmarksOutlinedIcon from "@mui/icons-material/BookmarksOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+
+import HomeIcon from "@mui/icons-material/Home";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MessageIcon from "@mui/icons-material/Message";
+import BookmarksIcon from "@mui/icons-material/Bookmarks";
+import ListIcon from "@mui/icons-material/List";
+
+export const filledIcons = {
+  Home: HomeIcon,
+  Notifications: NotificationsIcon,
+  Message: MessageIcon,
+  Bookmarks: BookmarksIcon,
+  Lists: ListIcon,
+};
+
+export const outlinedIcons = {
+  Home: HomeOutlinedIcon,
+  Notification: NotificationsNoneOutlinedIcon,
+  Message: MessageOutlinedIcon,
+  Bookmarks: BookmarksOutlinedIcon,
+  List: ListOutlinedIcon,
+};
+
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+
+export const NavbarIcons = {
+  home: HomeOutlinedIcon,
+  dark: DarkModeOutlinedIcon,
+  light: LightModeOutlinedIcon,
+};
+
+import InsertPhotoOutlinedIcon from "@mui/icons-material/InsertPhotoOutlined";
+import GifBoxOutlinedIcon from "@mui/icons-material/GifBoxOutlined";
+import EmojiEmotionsOutlinedIcon from "@mui/icons-material/EmojiEmotionsOutlined";
+import PollOutlinedIcon from "@mui/icons-material/PollOutlined";
+import AudiotrackIcon from "@mui/icons-material/Audiotrack";
+
+export const inputIcons = {
+  photo: InsertPhotoOutlinedIcon,
+  gif: GifBoxOutlinedIcon,
+  emoji: EmojiEmotionsOutlinedIcon,
+  poll: PollOutlinedIcon,
+  audio: AudiotrackIcon,
+};
