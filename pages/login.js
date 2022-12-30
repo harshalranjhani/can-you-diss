ba1fdd66bc516340fe01137c83fecac70c807c3d
Nakshatra Raghav <combatrickshaw@gmail.com>
2022-12-30 11:42:49 +0530

    ui: updated login page

--- pages/login.js
+++ pages/login.js
@@ -64,79 +64,114 @@ export default function SignIn() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
-            <LoginIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="div" noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={(evt) => {
-                setEmail(evt.target.value);
-              }}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={(evt) => {
-                setPassword(evt.target.value);
-              }}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              onClick={handleSubmit}
-              variant="outlined"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4, p: 2 }} />
-      </Container>
-    </ThemeProvider>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="border-2 border-border-gray p-8 rounded-lg space-y-2">
+        <h1 className="text-3xl font-black ">Can You Diss</h1>
+        <form className="flex flex-col">
+          <label>E-Mail</label>
+          <input
+            type="email"
+            className="input"
+            onChange={(evt) => {
+              setEmail(evt.target.value);
+            }}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            className="input"
+            onChange={(evt) => {
+              setPassword(evt.target.value);
+            }}
+          />
+        </form>
+        <div>
+          <Link href="/register">
+            <button className="button" onClick={handleSubmit}>
+              Login
+            </button>
+          </Link>
+        </div>
+
+        <div>
+          <p>Dont have an Account?</p>
+          <Link href="/register">Register</Link>
+        </div>
+      </div>
+    </div>
+    // <ThemeProvider theme={theme}>
+    //   <Container component="main" maxWidth="xs">
+    //     <CssBaseline />
+    //     <Box
+    //       sx={{
+    //         marginTop: 8,
+    //         display: "flex",
+    //         flexDirection: "column",
+    //         alignItems: "center",
+    //       }}
+    //     >
+    //       <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+    //         <LoginIcon />
+    //       </Avatar>
+    //       <Typography component="h1" variant="h5">
+    //         Sign in
+    //       </Typography>
+    //       <Box component="div" noValidate sx={{ mt: 1 }}>
+    //         <TextField
+    //           margin="normal"
+    //           required
+    //           fullWidth
+    //           id="email"
+    //           label="Email Address"
+    //           name="email"
+    //           autoComplete="email"
+    //           autoFocus
+    //           onChange={(evt) => {
+    //             setEmail(evt.target.value);
+    //           }}
+    //         />
+    //         <TextField
+    //           margin="normal"
+    //           required
+    //           fullWidth
+    //           name="password"
+    //           label="Password"
+    //           type="password"
+    //           id="password"
+    //           autoComplete="current-password"
+    //           onChange={(evt) => {
+    //             setPassword(evt.target.value);
+    //           }}
+    //         />
+    //         <FormControlLabel
+    //           control={<Checkbox value="remember" color="primary" />}
+    //           label="Remember me"
+    //         />
+    //         <Button
+    //           type="submit"
+    //           fullWidth
+    //           onClick={handleSubmit}
+    //           variant="outlined"
+    //           sx={{ mt: 3, mb: 2 }}
+    //         >
+    //           Sign In
+    //         </Button>
+    //         <Grid container>
+    //           <Grid item xs>
+    //             <Link href="#" variant="body2">
+    //               Forgot password?
+    //             </Link>
+    //           </Grid>
+    //           <Grid item>
+    //             <Link href="/register" variant="body2">
+    //               {"Don't have an account? Sign Up"}
+    //             </Link>
+    //           </Grid>
+    //         </Grid>
+    //       </Box>
+    //     </Box>
+    //     <Copyright sx={{ mt: 8, mb: 4, p: 2 }} />
+    //   </Container>
+    // </ThemeProvider>
   );
 }
